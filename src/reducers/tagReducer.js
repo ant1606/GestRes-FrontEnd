555a4c9521cd22fff5bd403b6f14f3531eb41966
Anthony <anthony.tp1606@gmail.com>
2022-11-04 16:05:25 -0500

    Generando tipos y reducer de TagReducer

--- src/reducers/tagReducer.js
+++ src/reducers/tagReducer.js
@@ -1,3 +1,5 @@
+import types from '../types/types';
+
 export const initialState = {
   tags: [],
   tagActive: null,
@@ -7,3 +9,44 @@ export const initialState = {
  * tags: Array, Conjunto de Tags
  * tagActive: Object, Tag a editar o eliminar
  */
+
+const tagReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.tagSave:
+      console.log('Guardando Tags');
+      return {
+        ...state,
+        tags: [payload, ...state.tags],
+      };
+
+    case types.tagSelect:
+      console.log('Seleccionando tag a actualizar/eliminar');
+      return {
+        ...state,
+        tagActive: payload,
+      };
+
+    case types.tagUpdate:
+      console.log('Actualizando tag');
+      return {
+        ...state,
+        tagActive: null,
+        tags: state.tags.map((tag) =>
+          tag.identificado === payload.identificador ? payload : tag
+        ),
+      };
+
+    case types.tagDelete:
+      console.log('Eliminando Tag');
+      return {
+        ...state,
+        tagActive: null,
+        tags: state.tags.filter((tag) => tag.identificador !== payload),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default tagReducer;
---
+++ src/types/types.js
@@ -0,0 +1,7 @@
+const types = {
+  tagSave: '[tag] Tag Saved',
+  tagSelect: '[tag] Set Tag to activeTag',
+  tagUpdate: '[tag] Tag Updated',
+  tagDelete: '[tag] Tag Deleted',
+};
+export default types;
