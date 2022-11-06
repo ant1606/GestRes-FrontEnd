b0d1ecd51b308c0765bb475ad7d0b65fbcbd2f99
Anthony <anthony.tp1606@gmail.com>
2022-11-06 18:46:43 -0500

    Agregando animaciones a validaciones de formulario.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Instalando react-paginate
 
 - docker run -it --rm --name myappnode -v "$PWD":/usr/src/app -w /usr/src/app node:16-alpine3.16 npm install react-paginate --save
+
+# Instalando animate.css
+
+- docker run -it --rm --name myappnode -v "$PWD":/usr/src/app -w /usr/src/app node:16-alpine3.16 npm install animate.css --save
--- src/Components/Atoms/Field.jsx
+++ src/Components/Atoms/Field.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
+import 'animate.css';
 
 const Field = ({ type, label, name, classBox = '', handleChange = null, errorInput = false }) => {
-  // className="px-3 py-1 border-b-2   bg-white peer w-full text-base transition-colors h-8
-  //       focus:outline-none"
-  // className="absolute left-0 -top-4 text-xs text-gray-600 cursor-text transition-all px-0
-  //       peer-placeholder-shown:top-1 peer-placeholder-shown:text-base peer-placeholder-shown:px-3
-  //       peer-focus:-top-4 peer-focus:text-xs peer-focus:text-gray-900 peer-focus:px-0"
+  //TODO: Verificar que el mensaje de error se agregue en esta seccion misma
   return (
-    <div className={`relative ${classBox}`}>
+    <div className={`${errorInput ? "animate__animated animate__headShake" : ""} relative ${classBox}`}>
       <input
         type={type}
         id={name}
         placeholder=" "
         name={name}
-        className={`${errorInput ? "border-2 border-rose-500 text-rose-500" : "border-gray-900"}
+        className={`${errorInput ? "border-2 border-rose-500 text-rose-500 " : "border-gray-900"}
         px-3 py-1  border-b-2 bg-white peer w-full text-base transition-colors h-8
         focus:outline-none`}
         onChange={handleChange}
