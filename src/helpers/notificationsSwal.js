b1b1ede0664f429f329fa9a57ad1de55886f8b40
Anthony <anthony.tp1606@gmail.com>
2022-11-21 19:11:33 -0500

    Limpiando espacios vacios

--- src/helpers/notificationsSwal.js
+++ src/helpers/notificationsSwal.js
@@ -1,15 +1,41 @@
 import Swal from "sweetalert2";
 
 export const toastNotifications = () => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showCloseButton: true,
+        showConfirmButton: false,
+        timer: 300000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     const toastError = () => {
-        Swal.fire({
-            position: 'top-end',
+        Toast.fire({
             icon: 'error',
-            title: `Ocurrio un error en el formulario.`,
-            showConfirmButton: false,
-            timer: 3000,
-            toast: true
-        });
+            title: 'ERROR',
+            text: 'Ocurrio un error en el formulario',
+            customClass: {
+                title: 'text-red-700',
+                popup: 'p-2'
+            }
+        })
+
+        // Swal.fire({
+        //     position: 'top-end',
+        //     icon: 'error',
+        //     title: `ERROR`,
+        //     text: `Ocurrio un error en el formulario.`,
+        //     color: 'red',
+        //     background: 'red',
+        //     showConfirmButton: false,
+        //     timer: 30000,
+        //     toast: true
+        // });
     }
 
     const toastSucces = () => {
