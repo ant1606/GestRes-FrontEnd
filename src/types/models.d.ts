d959fe7e8183b53b155b48ac06f219065ae50d10
Anthony <anthony.tp1606@gmail.com>
2023-07-22 12:28:42 -0500

    Definiendo las interface sd elos modelos Recourse, Progress y Status

--- src/types/models.d.ts
+++ src/types/models.d.ts
@@ -11,3 +11,37 @@ interface Tag {
   name: string;
   style: string;
 }
+
+interface Recourse {
+  id: number;
+  name: string;
+  source: string;
+  author: string | null;
+  editorial: string | null;
+  typeId: number;
+  totalPages: number | null;
+  totalChapters: number | null;
+  totalVideos: number | null;
+  totalHours: Date | null;
+  tags: Tag[];
+  status: Status[];
+  progress: Progress[];
+}
+
+interface Status {
+  id: number;
+  date: Date;
+  comment: string | null;
+  statusId: number;
+  recourseId: number;
+  statusName: string;
+}
+
+interface Progress {
+  id: number;
+  done: number;
+  pending: number;
+  date: Date;
+  comment: string;
+  recourseId: number;
+}
