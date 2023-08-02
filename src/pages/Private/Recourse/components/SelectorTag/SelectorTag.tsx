b66f9ebe8aa6846906746a206f47ccf94cbb2356
Anthony <anthony.tp1606@gmail.com>
2023-08-01 20:17:57 -0500

    Poblando el selector de Tags con los datos obtenidos desde la API

--- src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
+++ src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
@@ -1,29 +1,23 @@
 import Choices from 'choices.js';
 import React, { useEffect, useRef } from 'react';
 import 'choices.js/src/styles/choices.scss';
+import { getTags } from '@/services/tag.services';
 const SelectorTag: React.FC = () => {
   const selectRef = useRef(undefined);
 
   useEffect(() => {
     const choices = new Choices(selectRef.current, {
-      allowHTML: true,
-      removeItemButton: true
+      allowHTML: false,
+      placeholder: true,
+      placeholderValue: 'Pick an Strokes record',
+      maxItemCount: -1
+    }).setChoices(async function () {
+      const response = await getTags(`searchNombre=`);
+      return response.data.map((tag: Tag) => ({ value: tag.id, label: tag.name }));
     });
   }, []);
 
-  return (
-    <select multiple ref={selectRef}>
-      <option placeholder="true">Multiple Options</option>
-      <option defaultValue="2">Paris</option>
-      <option defaultValue="3">Bucharest</option>
-      <option defaultValue="4">Rome</option>
-      <option defaultValue="5">New York</option>
-      <option defaultValue="6">Miami</option>
-      <option defaultValue="7">Los Santos</option>
-      <option defaultValue="8">Sydney</option>
-      <option defaultValue="9">Piatra Neamt</option>
-    </select>
-  );
+  return <select multiple ref={selectRef} />;
 };
 
 export default SelectorTag;
