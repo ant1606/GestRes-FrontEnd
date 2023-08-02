45afe4fcdff38b7279fbce85c495b717150ddfe6
Anthony <anthony.tp1606@gmail.com>
2023-08-01 20:49:32 -0500

    Almacenando en el estado las TAgs seleccionadas

--- src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
+++ src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
@@ -1,20 +1,32 @@
 import Choices from 'choices.js';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'choices.js/src/styles/choices.scss';
 import { getTags } from '@/services/tag.services';
 const SelectorTag: React.FC = () => {
   const selectRef = useRef(undefined);
+  const [selectedValues, setSelectedValues] = useState<number[]>([]);
 
   useEffect(() => {
     const choices = new Choices(selectRef.current, {
       allowHTML: false,
       placeholder: true,
       placeholderValue: 'Pick an Strokes record',
+      removeItemButton: true,
       maxItemCount: -1
     }).setChoices(async function () {
       const response = await getTags(`searchNombre=`);
       return response.data.map((tag: Tag) => ({ value: tag.id, label: tag.name }));
     });
+
+    const handleChange = (event): void => {
+      setSelectedValues((prevState) => [...prevState, event.detail.value]);
+    };
+
+    selectRef.current.addEventListener('change', handleChange);
+    return () => {
+      // selectRef.current.removeEventListener('change', handleChange);
+      choices.destroy();
+    };
   }, []);
 
   return <select multiple ref={selectRef} />;
