0ca77856118a82803b0639f8f6e43d14302ef692
Anthony <anthony.tp1606@gmail.com>
2023-08-02 15:04:56 -0500

    Corrigiendo comportamiento del componente para extraer valores repetidos al eliminar un Tag de los seleccionados

--- src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
+++ src/pages/Private/Recourse/components/SelectorTag/SelectorTag.tsx
@@ -2,26 +2,39 @@ import Choices from 'choices.js';
 import React, { useEffect, useRef, useState } from 'react';
 import 'choices.js/src/styles/choices.scss';
 import { getTags } from '@/services/tag.services';
-const SelectorTag: React.FC = () => {
-  const selectRef = useRef(undefined);
-  const [selectedValues, setSelectedValues] = useState<number[]>([]);
+
+interface Props {
+  setSelectValues: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+const SelectorTag: React.FC<Props> = ({ setSelectValues }) => {
+  const selectRef = useRef<HTMLSelectElement>(undefined);
+  // const [selectedValues, setSelectedValues] = useState<number[]>([]);
 
   useEffect(() => {
     const choices = new Choices(selectRef.current, {
       allowHTML: false,
       placeholder: true,
-      placeholderValue: 'Pick an Strokes record',
+      placeholderValue: 'Selecciona las etiquetas',
       removeItemButton: true,
+      removeItems: true,
       maxItemCount: -1
     }).setChoices(async function () {
       const response = await getTags(`searchNombre=`);
       return response.data.map((tag: Tag) => ({ value: tag.id, label: tag.name }));
     });
 
-    const handleChange = (event): void => {
-      setSelectedValues((prevState) => [...prevState, event.detail.value]);
+    const handleChange = (event: CustomEvent): void => {
+      setSelectValues((prevState) => {
+        const existValue = prevState.find((val) => val === event.detail.value);
+        if (existValue === undefined || existValue === null)
+          return [...prevState, event.detail.value];
+
+        return prevState.filter((val) => val !== event.detail.value);
+      });
     };
 
+    // choices.passedElement.addEventListener('change', handleChange);
     selectRef.current.addEventListener('change', handleChange);
     return () => {
       // selectRef.current.removeEventListener('change', handleChange);
