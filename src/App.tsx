80c439cd62c040eccff6fa8cb1ac7d35a659d9e3
Anthony <anthony.tp1606@gmail.com>
2023-12-12 21:31:32 -0500

    fix: Cambiando <HashRouter/> por <BrowserRouter/> y añadiendo parametro -s a npm serve en Dockerfile de producción para la redirección a index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import { store } from '#/redux/store';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routers/AppRouter';
 
 // Se usa HashRouter por el tema de que npm serve no esta amanejando correctamente las rutas con BRowserRoute
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <AppRouter />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
