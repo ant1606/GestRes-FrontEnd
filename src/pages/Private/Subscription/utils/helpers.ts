c1b9ddbe1905bbebf4608305b07f19de24a347dc
Anthony <anthony.tp1606@gmail.com>
2024-01-01 20:09:54 -0500

    refact: Modulo SubscriptionYoutube, Cambiando la ruta de redirección de GoogleOAuth a una variable de entorno

--- src/pages/Private/Subscription/utils/helpers.ts
+++ src/pages/Private/Subscription/utils/helpers.ts
@@ -2,7 +2,7 @@ export const oauthSignIn = (): void => {
   const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
   const params = {
     client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-    redirect_uri: 'http://localhost:5173/oauthcallback',
+    redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT,
     response_type: 'token',
     scope: 'https://www.googleapis.com/auth/youtube.readonly',
     include_granted_scopes: 'true',
