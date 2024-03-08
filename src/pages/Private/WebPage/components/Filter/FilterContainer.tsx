91395ffbea776403a8c2ad6931dc532ffb3bc276
Anthony <anthony.tp1606@gmail.com>
2024-03-08 18:19:57 -0500

    fix: Aplicando TryCatch a componente FilterContainer de modulo WebPage Encapsulando el filtrado en un tryCatch para mostrar mensajes de errores en un modal

--- src/pages/Private/WebPage/components/Filter/FilterContainer.tsx
+++ src/pages/Private/WebPage/components/Filter/FilterContainer.tsx
@@ -9,6 +9,7 @@ import {
 } from '../../index.types';
 import { useDebounce } from '#/hooks/useDebounce';
 import { useFetch } from '#/hooks/useFetch';
+import { toastNotifications } from '#/utilities/notificationsSwal';
 
 export const FilterContainer: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -20,44 +21,47 @@ export const FilterContainer: React.FC = () => {
   const debouncedSearchNombre = useDebounce(searchNombre);
 
   const execFilter = async (): Promise<void> => {
-    // try {
-    searchParams.delete('searchNombre');
-    searchParams.delete('perPage');
-    searchParams.delete('searchTags[]');
-    searchParams.append('perPage', webPagePerPage);
-    searchParams.delete('page');
-    searchParams.append('page', '1');
-    if (debouncedSearchNombre !== '') searchParams.append('searchNombre', debouncedSearchNombre);
-    if (searchTags.length > 0) searchParams.append('searchTags[]', searchTags.toString());
+    try {
+      searchParams.delete('searchNombre');
+      searchParams.delete('perPage');
+      searchParams.delete('searchTags[]');
+      searchParams.append('perPage', webPagePerPage);
+      searchParams.delete('page');
+      searchParams.append('page', '1');
+      if (debouncedSearchNombre !== '') searchParams.append('searchNombre', debouncedSearchNombre);
+      if (searchTags.length > 0) searchParams.append('searchTags[]', searchTags.toString());
 
-    searchParams.sort();
-    setSearchParams(searchParams);
-    const response = await getWebPages(searchParams.toString(), fetchWithSessionHandling);
+      searchParams.sort();
+      setSearchParams(searchParams);
+      const response = await getWebPages(searchParams.toString(), fetchWithSessionHandling);
 
-    if (response.status === 'error') {
-      const responseError = response as WebPagePaginatedErrorResponse;
+      if (response.status === 'error') {
+        const responseError = response as WebPagePaginatedErrorResponse;
 
-      // Mensaje de error general por parte del backend
-      if (responseError.message !== '') {
-        throw new Error(responseError.message);
+        // Mensaje de error general por parte del backend
+        if (responseError.message !== '') {
+          throw new Error(responseError.message);
+        }
+
+        // Para que no interrumpa el UX, seríá mejor validar el campo de search en frontend o agregar un boton de busqueda
+        // TODO Son errores de validación de campos, ver si se maneja el filtrado como un formulario para mostrar los errores
+        // en los inputs
+        if (!Object.values(responseError.details).every((value) => value === null)) {
+          const message = Object.values(responseError.details).reduce((acc, val) => {
+            if (val !== null) {
+              acc += val + ' ';
+            }
+            return acc;
+          }, '');
+
+          throw new Error(message);
+        }
+      } else {
+        const responseSuccess = response as WebPagesPaginatedSuccessResponse;
+        setWebPages(responseSuccess);
       }
-
-      // Para que no interrumpa el UX, seríá mejor validar el campo de search en frontend o agregar un boton de busqueda
-      // TODO Son errores de validación de campos, ver si se maneja el filtrado como un formulario para mostrar los errores
-      // en los inputs
-      if (!Object.values(responseError.details).every((value) => value === null)) {
-        const message = Object.values(responseError.details).reduce((acc, val) => {
-          if (val !== null) {
-            acc += val + ' ';
-          }
-          return acc;
-        }, '');
-
-        throw new Error(message);
-      }
-    } else {
-      const responseSuccess = response as WebPagesPaginatedSuccessResponse;
-      setWebPages(responseSuccess);
+    } catch (error: any) {
+      toastNotifications().notificationError(error.message);
     }
   };
 
